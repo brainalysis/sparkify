16caf0d7dcac204868fbd057dd62de0da941dd17
Fahad Akbar <fahad.akbar@gmail.com>
2022-04-08 18:09:26 +0000

    bug fix pytest preprocessing

--- tests/preprocessing/test_pipe_numeric_only.py
+++ tests/preprocessing/test_pipe_numeric_only.py
@@ -8,30 +8,28 @@ spark = SparkSession.builder.appName("test_pipeline").getOrCreate()
 
 @pytest.fixture
 def sample_data_train():
-    df = spark.createDataFrame([
-      (1,1,.24),
-      (0,2,.56),
-      (0,3,.12),
-      (1,4,.39)
-    ],
-      ['col_a','col_b','label'])
-    
-    # df = ps.DataFrame(dict(col_a=[1,2,3,4],col_b=[.24,.56,.12,.39],label=[1,0,0,1]))
-    
+    # df = spark.createDataFrame([
+    #   (1,1,.24),
+    #   (0,2,.56),
+    #   (0,3,.12),
+    #   (1,4,.39)
+    # ],
+    #   ['col_a','col_b','label'])
+    df = ps.DataFrame(dict(col_a=[1,2,3,4],col_b=[.24,.56,.12,.39],label=[1,0,0,1])).to_spark()
     return df
 
 @pytest.fixture
 def sample_data_holdout():
-      df = spark.createDataFrame([
-       (1,1,.24),
-       (0,2,.56),
-       (0,3,.12),
-       (1,4,.39)
-        ],
-      ['col_a','col_b','label'])
+      # df = spark.createDataFrame([
+      #  (1,1,.24),
+      #  (0,2,.56),
+      #  (0,3,.12),
+      #  (1,4,.39)
+      #   ],
+      # ['col_a','col_b','label'])
     
-    # df = ps.DataFrame(dict(col_a=[1,2,3,4],col_b=[.24,.56,.12,.39],)).to_spark()
-      return df
+    df = ps.DataFrame(dict(col_a=[5,6,,8],col_b=[.30,.80,.32,],label=[1,0,0,])).to_spark()
+    return df
                                                                               
 def test_numeric_only(sample_data_train ,sample_data_holdout):
                                                                               
