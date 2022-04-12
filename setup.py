8fdc2c98c21c3aee5b3e7d6c0572dad80367a597
Fahad Akbar <fahadakbar@gmail.com>
2022-04-11 21:01:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=["pandas", "numpy", "pyspark>=3.2.1", "pytest"],
+    install_requires=["pandas", "numpy", "pyspark >= 3.2.1", "pytest"],
     keywords=["python", "spark", "machine learning",],
     classifiers=[
         "Development Status :: 1 - Planning",
