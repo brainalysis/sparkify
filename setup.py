18ef2eb6f02cd2527e9e94bd3c2033109a6f9b8d
Fahad Akbar <fahadakbar@gmail.com>
2022-04-15 23:09:50 -0400

    Long description changed

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
+#with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+#    long_description = "\n" + fh.read()
 
 VERSION = "0.0.6"
 DESCRIPTION = "For an easy implementation of spark's ml  "
@@ -26,7 +26,7 @@ setup(
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8.12",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
