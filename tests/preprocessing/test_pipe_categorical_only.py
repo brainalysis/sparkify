a96ac67f7e2adf0d42d4ff20ae9cf7a218b6c226
fahad_linux <fahadakbar@gmail.com>
2022-05-07 00:28:00 -0400

    adding preprocessing test

--- tests/preprocessing/test_pipe_categorical_only.py
+++ tests/preprocessing/test_pipe_categorical_only.py
@@ -7,7 +7,12 @@ from SparkAutoML.ml_module.preprocessing_module.preprocess_file import Preproces
 @pytest.fixture
 def sample_data_train():
     df = ps.DataFrame(
-        dict(col_a=[1, 2, 3, 4], col_b=[0.24, 0.56, 0.12, 0.39], label=[1, 0, 0, 1])
+        dict(
+            col_a=[1, 2, 3, 4],
+            col_b=[0.24, 0.56, 0.12, 0.39],
+            col_cat=["a", "a", "a", "b"],
+            label=[1, 0, 0, 1],
+        )
     ).to_spark()
     return df
 
@@ -15,25 +20,32 @@ def sample_data_train():
 @pytest.fixture
 def sample_data_holdout():
     df = ps.DataFrame(
-        dict(col_a=[5, 6, 8], col_b=[0.30, 0.80, 0.32,], label=[1, 0, 0,])
+        dict(
+            col_a=[5, 6, 8],
+            col_b=[0.30, 0.80, 0.32,],
+            col_cat=["a", "a", "b"],
+            label=[1, 0, 0,],
+        )
     ).to_spark()
     return df
 
 
 @pytest.fixture
 def sample_data_unseen():
-    df = ps.DataFrame(dict(col_a=[5, 6, 8], col_b=[0.30, 0.80, 0.32,],)).to_spark()
+    df = ps.DataFrame(
+        dict(col_a=[5, 6, 8], col_b=[0.30, 0.80, 0.32,], col_cat=["a", "a", "c"],)
+    ).to_spark()
     return df
 
 
-def test_numeric_only(sample_data_train, sample_data_holdout, sample_data_unseen):
+def test_category_only(sample_data_train, sample_data_holdout, sample_data_unseen):
 
     pipe = Preprocessor(
         training_data=sample_data_train,
         hold_out_data=sample_data_holdout,
         target_feature="label",
-        numeric_features=["col_a", "col_b"],
-        categorical_features=None,
+        numeric_features=None,
+        categorical_features=["col_cat"],
     )
 
     # execute the pipeline
